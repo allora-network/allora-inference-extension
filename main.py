7d6f4b3d651f19fbfcb2c07eac0b9fbd366a4b6c
Derek Anderson <dmikey@users.noreply.github.com>
2024-04-25 10:04:17 -0500

    worker should return an inference value and forecast value (#6)
    * worker should return an inference value and forecast value
    
    Signed-off-by: Derek Anderson <derek@upshot.xyz>
    
    * mimick the shape we discussed in slack
    
    Signed-off-by: Derek Anderson <derek@upshot.xyz>
    
    ---------
    
    Signed-off-by: Derek Anderson <derek@upshot.xyz>

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import sys
 import random
 import json
 
-# An inference example as a random number.
-random_number = random.randint(1, 100)
-
- # Create Dictionary
+# Create Dictionary
 value = {
-    "value": str(random_number)
+    "infererValue": str(random.randint(1, 100)), 
+    "forecasterValues":  [
+		{
+			"node":"allo1inf1",
+			"value":str(random.randint(1, 100))
+		},
+		{
+			"node":"allo1inf2",
+			"value":str(random.randint(1, 100))
+		},
+		{
+			"node":"allo1inf1111",
+			"value":str(random.randint(1, 100))
+		}
+	]
 }
 
 print(json.dumps(value))
